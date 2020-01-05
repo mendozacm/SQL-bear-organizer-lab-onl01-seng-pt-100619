cc59b1ac6b2d55922aa4e7fe00d82d3eebda676d
Cesar Mendoza <mendoza.cesar92@gmail.com>
2020-01-05 01:29:32 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,17 +1,17 @@
 INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
     VALUES (1, "Mr. Chocolate", 3, "male", "brown", "rowdy", TRUE);
-    INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
+INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
     VALUES (2, "Rowdy", 3, "female", "black", "complacent", TRUE);
-    INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
+INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
     VALUES (3, "Tabitha", 5, "female", "yellow", "calm", TRUE);
-    INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
+INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
     VALUES (4, "Sergeant Brown", 12, "male", "white", "loud", TRUE);
-    INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
+INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
     VALUES (5, "Melissa", 13, "female", "brown", "sassy", False);
-    INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
+INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
     VALUES (6, "Grinch", 8, "male", "blue", "joyful", TRUE);
-    INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
+INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
     VALUES (7, "Wendy", 14, "female", "brown", "happy", False);
-    INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
+INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
     VALUES (8, NULL, 3, "male", "brown", "rowdy", FALSE);
     
