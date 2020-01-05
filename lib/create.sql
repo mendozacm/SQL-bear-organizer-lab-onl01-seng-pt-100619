3b5a1dab988a4e94f596f3ddc2880f708fbf99c5
Cesar Mendoza <mendoza.cesar92@gmail.com>
2020-01-05 02:00:07 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  ID INTEGER NOT NULL PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  gender TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive INTEGER
+);
---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (ID, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 3, "male", "brown", "rowdy", 0);
+INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
+    VALUES (2, "Rowdy", 3, "female", "black", "complacent", 1);
+INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
+    VALUES (3, "Tabitha", 5, "female", "yellow", "calm", 1);
+INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
+    VALUES (4, "Sergeant Brown", 12, "male", "white", "loud", 1);
+INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
+    VALUES (5, "Melissa", 13, "female", "brown", "sassy", 0);
+INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
+    VALUES (6, "Grinch", 8, "male", "blue", "joyful", 1);
+INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
+    VALUES (7, "Wendy", 14, "female", "brown", "happy", 0);
+INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
+    VALUES (8, NULL, 3, "male", "brown", "rowdy", 0);
+    
