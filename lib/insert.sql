8245dc5de84dbe8cf0f4e359b66484448a3d3daf
Cesar Mendoza <mendoza.cesar92@gmail.com>
2020-01-05 01:37:01 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -6,11 +6,11 @@ INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
 INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
     VALUES (4, "Sergeant Brown", 12, "male", "white", "loud", 1);
 INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
-    VALUES (5, "Melissa", 13, "female", "brown", "sassy", False);
+    VALUES (5, "Melissa", 13, "female", "brown", "sassy", 0);
 INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
-    VALUES (6, "Grinch", 8, "male", "blue", "joyful", TRUE);
+    VALUES (6, "Grinch", 8, "male", "blue", "joyful", 1);
 INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
-    VALUES (7, "Wendy", 14, "female", "brown", "happy", False);
+    VALUES (7, "Wendy", 14, "female", "brown", "happy", 0);
 INSERT INTO bears (ID, name, age, gender, color, temperament, alive)
-    VALUES (8, NULL, 3, "male", "brown", "rowdy", FALSE);
+    VALUES (8, NULL, 3, "male", "brown", "rowdy", 0);
     
